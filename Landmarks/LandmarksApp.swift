8d6a622ea76ad2cb650a5479c84ad2c26d7b4bed
aglegaspi <lex83nyc@gmail.com>
2021-11-18 17:53:01 -0800

    create and combine views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Alex 6.1 on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
