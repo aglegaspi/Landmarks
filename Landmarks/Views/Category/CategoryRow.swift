64ff5dbc14c893fe8a2ce0b7019b66e8ae277d2f
aglegaspi <lex83nyc@gmail.com>
2021-11-21 13:48:16 -0800

    create CategoryRow and show category name and landmarks

---
+++ Landmarks/Views/Category/CategoryRow.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Alex 6.1 on 11/21/21.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var categoryName: String
+    var items: [Landmark]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(categoryName)
+                .font(.headline)
+            
+            HStack(alignment: .top, spacing: 0) {
+                ForEach(items) { landmark in
+                    Text(landmark.name)
+                }
+            }
+        }
+        
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
+    static var previews: some View {
+        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(3)))
+    }
+}
