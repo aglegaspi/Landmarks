4ebaf4643aee930164c3da69929e01ea3d9e9e0a
aglegaspi <lex83nyc@gmail.com>
2021-11-21 13:21:53 -0800

    add computed categories dictionary

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -10,6 +10,10 @@ import Foundation
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarks) { $0.category.rawValue}
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
