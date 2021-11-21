637574668125420827ed9dc827ac64d6256765b3
aglegaspi <lex83nyc@gmail.com>
2021-11-21 13:52:58 -0800

    add spacing and test scroll view with more data

--- Landmarks/Views/Category/CategoryRow.swift
+++ Landmarks/Views/Category/CategoryRow.swift
@@ -15,12 +15,17 @@ struct CategoryRow: View {
         VStack(alignment: .leading) {
             Text(categoryName)
                 .font(.headline)
+                .padding(.leading, 15)
+                .padding(.top, 5)
             
-            HStack(alignment: .top, spacing: 0) {
-                ForEach(items) { landmark in
-                    Text(landmark.name)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(alignment: .top, spacing: 0) {
+                    ForEach(items) { landmark in
+                        Text(landmark.name)
+                    }
                 }
             }
+            .frame(height: 185)
         }
         
     }
@@ -30,6 +35,6 @@ struct CategoryRow_Previews: PreviewProvider {
     static var landmarks = ModelData().landmarks
     
     static var previews: some View {
-        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(3)))
+        CategoryRow(categoryName: landmarks[0].category.rawValue, items: Array(landmarks.prefix(5)))
     }
 }
