f4d54d4d77a3f89a1569b9b6be0fb6cf54fe1d19
aglegaspi <lex83nyc@gmail.com>
2021-11-23 09:57:49 -0800

    create tabview with tab modifiers and give each tab a label

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -11,11 +11,25 @@ struct ContentView: View {
     @State private var selection: Tab = .featured
     
     enum Tab {
-        case featured, list
+        case featured
+        case list
     }
     
     var body: some View {
+        TabView(selection: $selection) {
+            CategoryHome()
+                .tabItem {
+                    Label("Featured", systemImage: "star")
+                }
+                .tag(Tab.featured)
+            
             LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+        }
+            
     }
 }
 
