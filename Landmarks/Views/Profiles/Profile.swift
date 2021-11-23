e3b7941b4038fc8ce679097726b17f9eeb2682f8
aglegaspi <lex83nyc@gmail.com>
2021-11-23 10:11:35 -0800

    create Profile model

---
+++ Landmarks/Views/Profiles/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Alex 6.1 on 11/23/21.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var perfersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_alxndr")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { self.rawValue }
+    }
+}
