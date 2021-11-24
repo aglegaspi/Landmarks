b7bdc196c7c636e63dff39b1db2c6f061f644279
aglegaspi <lex83nyc@gmail.com>
2021-11-24 11:12:23 -0800

    Add the coordinator as the target for the valueChanged event, specifying the updateCurrentPage(sender:) method as the action to perform.

--- Landmarks/Views/PageView/PageControl.swift
+++ Landmarks/Views/PageView/PageControl.swift
@@ -12,13 +12,14 @@ struct PageControl: UIViewRepresentable {
     var numberOfPages: Int
     @Binding var currentPage: Int
     
-    func makeCoordinator() -> () {
+    func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
     
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
         
         return control
     }
