a955beaddb78cb377c236ce2eccf2ec55c468a12
aglegaspi <lex83nyc@gmail.com>
2021-11-25 10:33:58 -0800

    Add the modelData, landmark, and landmarkIndex properties to to the new LandmarkDetail structure.

---
+++ WatchLandmarks WatchKit Extension/LandmarkDetail.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkDetail.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Alex 6.1 on 11/25/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.first(where: { $0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: ModelData().landmarks[0])
+    }
+}
