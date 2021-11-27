5fd5d8d12af683cbb8afd346ba6c82e53bb561b1
aglegaspi <lex83nyc@gmail.com>
2021-11-27 08:58:11 -0800

    add macOS target and remove MacLandmarksApp

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Alex 6.1 on 11/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
