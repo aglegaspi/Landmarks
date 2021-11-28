879c312cb0dd46ef1ebc3fd6f6f1c9fe2d6364a3
aglegaspi <lex83nyc@gmail.com>
2021-11-27 20:41:05 -0800

    create LandmarkCommands with Targets for iOS and macOS

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Alex 6.1 on 11/27/21.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        
+    }
+}
