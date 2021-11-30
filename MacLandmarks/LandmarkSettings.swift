e7f69adbe3c057b685f8d67e76a5dc6b60986dd8
aglegaspi <lex83nyc@gmail.com>
2021-11-30 09:59:11 -0800

    create LandmarkSetting with a Target for macOS

---
+++ MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkSettingss.swift
+//  MacLandmarks
+//
+//  Created by Alex 6.1 on 11/30/21.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
